f470d2d7d99fe27750f07743566c43c324a17029
abdurrahim220 <z.abdurrahim5@gmail.com>
2025-01-31 11:53:28 +0600

    backend setup compete

---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import { config } from './config';
+import app from './app';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log('Database connected');
+  } catch (error) {
+    console.log('Failed to connect database', error);
+  }
+};
+
+app.listen(config.port, () => {
+  console.log(`Server is running on port ${config.port}`);
+});
+
+connectDB();
